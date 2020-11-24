47e017af4b3d14272b433508ae7acf41587f10c5
Piplo31 <juan.pablo.salgado@correounivalle.edu.co>
2020-11-24 17:31:17 -0500

    agregue el controlador de laboratorio con la idea de qu en vez de recibir el id de la ciudad recibe el nombre de esta

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import eps from "./eps";
+import laboratorio from "./laboratorio";
 import medicamento from "./medicamento";
+import tipoContacto from "./tipoContacto";
 import tipoIdentificacion from "./tipoIdentificacion";
 import tipoTelefono from "./tipoTelefono";
 import universidad from "./universidad";
-import tipoContacto from "./tipoContacto";
 const routes = Router();
 
 //Rutas
@@ -14,5 +15,6 @@ routes.use('/tipo-telefono', tipoTelefono);
 routes.use('/EPS', eps);
 routes.use('/medicamento', medicamento);
 routes.use('/tipo-contacto', tipoContacto);
+routes.use('/laboratorio', laboratorio);
 
 export default routes;
